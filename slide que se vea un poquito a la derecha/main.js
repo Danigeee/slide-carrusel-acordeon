ceb8cb3fc71bab6f4d6e4d34bb39cb47d930bac3
Danigeee <daniav9924@gmail.com>
2023-01-06 08:12:19 -0500

    first commit

---
+++ slide que se vea un poquito a la derecha/main.js
@@ -0,0 +1,8 @@
+$('.xxx-sliders-container').slick({
+    slidesToShow: 2,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+
+    // para que se vea un poquito a la izquierda
+    variableWidth: true,
+})
---
+++ slide que se vea un poquito a la izquierda/main.js
@@ -0,0 +1,10 @@
+$('.xxx-sliders-container').slick({
+    slidesToShow: 2,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    initialSlide:1,
+    // para que se vea un poquito a la izquierda
+    variableWidth: true,
+
+    centerMode: true,
+})
